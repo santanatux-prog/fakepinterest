fcf29f5cc7e2662339f74f6330abb6ad951f08b3
Santana <santana.tux@gmail.com>
2025-10-05 17:05:11 -0300

    Init - criacao do banco

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
+import sqlalchemy
+
 
 # Criação da aplicação
 app = Flask(__name__)
@@ -20,5 +22,18 @@ database = SQLAlchemy(app) # Cria o banco de dados dentro da aplicação
 bcrypt = Bcrypt(app) # Faz a criptografia de senhas dos usuários
 login_manager = LoginManager(app) # Faz o gerenciamento dos Logins
 login_manager.login_view = "homepage" # Direciona para pagina que faz login, caso nenhum usuário tiver logado
+
+# Avalia e cria o banco de dados no servidor remoto se não existir
+from fakepinterest import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspect = sqlalchemy.inspect(engine)
+
+if not inspect.has_table("usuario"):
+    with app.app_context():
+        #database.drop_all()
+        database.create_all()
+        print("Base de dados criada com sucesso.")
+else:
+    print("Base de dados existente")
 
 from fakepinterest import routes
