7c25cfd2f0af3fd809116a432fbd5227b8c1fed3
Santana <santana.tux@gmail.com>
2025-10-05 14:36:06 -0300

    debug false

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from fakepinterest import app
 
 # Coloca o site no ar no modo debug, chamando o __init__ do diretorio fakepinterest
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
